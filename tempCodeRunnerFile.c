2be3c5eb80ca713893412fc1117d1c47cb3ec70b
Raymond Biko <raybiko@gmail.com>
2023-03-09 23:19:49 +0300

    ring bell

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,5 @@
 #include<stdio.h> 
 int main(){
-    printf("This is line one \n"); 
-    printf("This is line two \n"); 
-    printf("This is line three"); 
-    return 0;
+    printf("\a");
+    return 0; 
 }
