27ecbbbbbc0acd824a5d2616d9b30b5e89f48be3
Raymond Biko <raybiko@gmail.com>
2023-03-09 22:51:32 +0300

    Variable

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,10 @@
-#include<stdio.h>
+#include<stdio.h> 
 int main(){
-	printf("This is a C program \n");
-	return 0; 
+    int num; /*Defines a variable called num*/
+    num = 1;	/*Assigment*/ 
+    printf("This is a simple program");
+	printf(" to display a message. \n");
+	printf("My favorite number is %d because ",num);
+	printf(" it is first.\n ");
+	return 0;
 }
