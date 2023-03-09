519373bf4a5d70e270c044041272c334844f6cea
Raymond Biko <raybiko@gmail.com>
2023-03-09 23:08:38 +0300

    print line

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,7 @@
 #include<stdio.h> 
 int main(){
-    int num; /*Defines a variable called num*/
-    num = 1;	/*Assigment*/ 
-    printf("This is a simple program");
-	printf(" to display a message. \n");
-	printf("My favorite number is %d because ",num);
-	printf(" it is first.\n ");
-	return 0;
+    printf("This is line one \n"); 
+    printf("This is line two \n"); 
+    printf("This is line three"); 
+    return 0;
 }
