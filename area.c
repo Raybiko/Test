8a184f9a50959309d8238db0821305b0ba0caf27
Raymond Biko <raybiko@gmail.com>
2023-03-10 10:12:39 +0300

    Cprogs

---
+++ area.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main()
+{
+	int len, width;
+	printf("\n Enter length: ");
+	scanf ("%d ", &len);
+	printf("\n Enter width : " );
+	scanf( " %d ", &width);
+	printf("\n The area is %d ", len * width);
+	return 0; 
+}
---
+++ printing.c
@@ -0,0 +1,7 @@
+#include<stdio.h> 
+int main(){
+    printf("This is line one \n"); 
+    printf("This is line two \n"); 
+    printf("This is line three"); 
+    return 0;
+}
